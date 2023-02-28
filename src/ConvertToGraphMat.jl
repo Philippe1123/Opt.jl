1cdea703cd75dd6b878963392aaa95db98e91c2b
philippe1123 <blondeel.philippe@gmail.com>
2023-02-28 10:44:50 +0100

    Finally working bidir

--- src/ConvertToGraphMat.jl
+++ src/ConvertToGraphMat.jl
@@ -9,7 +9,7 @@ using PyPlot
 function run()
 
 
-    nameOfFile = "/home/philippe/.julia/dev/Opt/src/JSP_dataset_small.xlsx"
+    nameOfFile = "/home/philippe/.julia/dev/Opt/src/JSP_dataset_LA19.xlsx"
     xf = XLSX.readxlsx(nameOfFile)
     n = xf["Parameters!B2"]
     m = xf["Parameters!B3"]
@@ -22,14 +22,14 @@ function run()
 
 
 
-    mat = Matrix(transpose(reshape(2:n*m+1, n, m)))
+    mat = Matrix(transpose(reshape(2:n*m+1, m, n)))
     GraphMat = zeros(n * m + 2, n * m + 2)
     TimesMat = zeros(n * m + 2, n * m + 2)
     TimesDict = Dict()
 
     for j = 1:n
         for i = 1:m
-            TimesDict[mat[i, j]] = times[i, j]
+            TimesDict[mat[j, i]] = times[j, i]
         end
     end
 
@@ -38,7 +38,6 @@ function run()
 
             GraphMat[mat[j, i], mat[j, i+1]] = 1
             TimesMat[mat[j, i], mat[j, i+1]] = times[j, i]
-            println(GraphMat[mat[j, i], mat[j, i+1]])
 
         end
     end
@@ -47,10 +46,7 @@ function run()
     for j = 1:n
         for i = 2:m
             TimesMat[mat[1,1]-1,mat[j, i]] = TimesMat[mat[1,1]-1,mat[j, i-1]] + TimesMat[mat[j,i]-1,mat[j, i]]
-            println([mat[1,1]-1,mat[j, i]] )
-            println([mat[1,1]-1,mat[j, i-1]])
-            println([mat[j,i]-1,mat[j, i]])
-            println("---")
+
 
         end
     end
@@ -74,7 +70,6 @@ function run()
 
         GraphMat[mat[j, m], n*m+2] = 1
         TimesMat[mat[j, m], n*m+2] = times[j, end]
-        #println(GraphMat[mat[j,m],n*m+2])
 
 
     end
@@ -83,7 +78,6 @@ function run()
     for j = 1:n
 
         GraphMat[1, mat[j, 1]] = 1
-        #println(1,GraphMat[mat[j,m]])
 
 
     end
@@ -94,14 +88,12 @@ function run()
 
             for p = j+1:n
                 for k = 1:m
-                    println([j, i, p, k])
                     if (out == machines[p, k])
                         GraphMat[mat[j, i], mat[p, k]] = 2
                         GraphMat[mat[p, k], mat[j, i]] = 2
                     end
                 end
             end
-            println("---")
 
 
         end
@@ -109,7 +101,6 @@ function run()
 
 
 
-    @show GraphMat
 
     Bidir(GraphMat, TimesMat, TimesDict, mat, 3,machines)
 
@@ -164,15 +155,8 @@ function Bidir(Graph::Matrix, TimesMat::Matrix, TimesDict::Dict, Jobs::Matrix, n
         t[pos[op]] = 0
 
 
-        #TimesMatInv[pos[op], PJ[pos[op]]] =
-            #TimesMatInv[pos[op], PJ[pos[op]]] + TimesDict[PJ[pos[op]]]
-
-
-
         t[pos[op]] = t[pos[op]] + TimesMatInv[pos[op], end] # machine precedence constraint
-        #TimesMatInv[end,pos[op]] = 0
         t[pos[op]] = t[pos[op]] + TimesMatInv[end, pos[op]] # job precedence constraint
-        #TimesMatInv[pos[op],end] = 0
 
     end
 
@@ -190,10 +174,10 @@ function Bidir(Graph::Matrix, TimesMat::Matrix, TimesDict::Dict, Jobs::Matrix, n
         pos = findall(x -> x == 2, A)
 
         ln = length(pos)
-        println(string("timesmast[1,9]",TimesMat[1,9]))
         for op = 1:ln
             Sol[operation_under_consideration, pos[op]] = 3#forward
-            println(TimesMat[pos[op], operation_under_consideration])
+            Sol[pos[op], operation_under_consideration] = 0
+
 
             if TimesMat[1, pos[op]] < TimesMat[1, operation_under_consideration] + TimesDict[operation_under_consideration]
                 TimesMat[1, pos[op]] = TimesMat[1, operation_under_consideration] + TimesDict[operation_under_consideration]
@@ -208,7 +192,7 @@ function Bidir(Graph::Matrix, TimesMat::Matrix, TimesDict::Dict, Jobs::Matrix, n
                     for jk = 1 :length(mac_cnstr)
                         TimesMat[Jobs[1,1]-1,mac_cnstr[jk]] = TimesMat[Jobs[1,1]-1,mac_cnstr[jk]] + TimesDict[operation_under_consideration]
 
-                        ### some recursive shit now
+                        ### some recursive function now ?
 
 
                         ### end recursvie
@@ -219,9 +203,7 @@ function Bidir(Graph::Matrix, TimesMat::Matrix, TimesDict::Dict, Jobs::Matrix, n
 
                 end
             end
-            println(TimesMat[pos[op], operation_under_consideration])
 
-            Sol[pos[op], operation_under_consideration] = 0
         end
         append!(L, operation_under_consideration)
         remove!(T, operation_under_consideration)
@@ -282,31 +264,29 @@ function Bidir(Graph::Matrix, TimesMat::Matrix, TimesDict::Dict, Jobs::Matrix, n
 
             ln = length(pos)
 
-            if operation_under_consideration == 6
-                println("7")
-            end
+
 
 
 
 
             for op = 1:ln
                 Sol[operation_under_consideration, pos[op]] = 0#backward
-                println(TimesMatInv[pos[op], operation_under_consideration])
-    
-                if TimesMatInv[end, pos[op]] < TimesMatInv[end, operation_under_consideration] + TimesDict[operation_under_consideration]
-                    TimesMatInv[end, pos[op]] = TimesMatInv[end, operation_under_consideration] + TimesDict[operation_under_consideration]
-    
+                Sol[pos[op], operation_under_consideration] = 3
+
+                
+                if -TimesMatInv[end, pos[op]] + TimesDict[pos[op]] > -TimesMatInv[end, operation_under_consideration] 
+                    TimesMatInv[end, pos[op]] = TimesMatInv[end, operation_under_consideration] + TimesDict[pos[op]]
                     out = (findall(x -> x == pos[op], Jobs))
                     for i = out[1][2]:-1:2
-                        TimesMatInv[Jobs[end,end]+1,Jobs[out[1][1], i-1]] = TimesMatInv[Jobs[end,end]+1,Jobs[out[1][1], i-1]] + TimesDict[operation_under_consideration]
-                        mac_cnstr = (findall(x -> x == 4, Sol[Jobs[out[1][1], i-1],:]))  
-                        #for jk = 1 :length(mac_cnstr) # to be fixed
-                        #    TimesMatInv[Jobs[end,end]+1,mac_cnstr[jk]] = TimesMatInv[Jobs[end,end]+1,mac_cnstr[jk]] + TimesDict[operation_under_consideration]
-                        #end
+                        TimesMatInv[Jobs[end,end]+1,Jobs[out[1][1], i-1]] = TimesMatInv[Jobs[end,end]+1,Jobs[out[1][1], i-1]] + TimesDict[pos[op]]
+                        mac_cnstr = (findall(x -> x == 3, Sol[:,Jobs[out[1][1], i-1]]))  
+                        for jk = 1 :length(mac_cnstr) # to be fixed
+                           TimesMatInv[Jobs[end,end]+1,mac_cnstr[jk]] = TimesMatInv[Jobs[end,end]+1,mac_cnstr[jk]] + TimesDict[pos[op]]
+                        end
                     end
                 end
+
     
-                Sol[pos[op], operation_under_consideration] = 4
             end
             
             append!(R, operation_under_consideration)
@@ -324,7 +304,7 @@ function Bidir(Graph::Matrix, TimesMat::Matrix, TimesDict::Dict, Jobs::Matrix, n
             for el = 1:length(T)
 
 
-                out = (findall(x -> x == 4, Sol[T[el], :]))
+                out = (findall(x -> x == 3, Sol[T[el], :]))
                 if length(out) > 0
                     interm = []
                     for ip = 1:size(out, 1)
@@ -362,104 +342,43 @@ function Bidir(Graph::Matrix, TimesMat::Matrix, TimesDict::Dict, Jobs::Matrix, n
 
 
     end
-    println(L)
-    println(R)
-    println(r)
-    println(t)
-    @show TimesMatInv
-    @show TimesMat
-    @show Sol
 
-    sumtime = Dict()
-    sumvec = []
-    for kl in keys(r)
-        for jk in keys(t)
-            if kl == jk
-                sumtime[kl] = TimesMatInv[end,kl] + TimesMat[1,kl]
-                append!(sumvec,sumtime[kl])
 
-            end
-        end
-    end
 
-    @show sumtime
-    @show maximum(sumvec)
+
+
+
+    maxtimeNew= maximum(TimesMat[1,L]) + maximum(TimesMatInv[end,R])
+
+    ot = transpose(vcat(TimesMat[1,:]',collect(1:length(TimesMat[1,:]))'))
+    ot = ot[L,:]
+    ot = ot[sortperm(ot[:,1]),:]
+    pt = ot[end,2]
+
+
     figure()
-    maxtime = maximum(sumvec)
     deleteat!(L, 1)
-    println(R)
     deleteat!(R, 1)
-
+    maxtime = maxtimeNew + TimesDict[pt]
     for kl in L
         out = (findall(x -> x == kl, Jobs))
-        println(kl)
-        println(TimesDict)
-        println(TimesDict[kl])
 
-        #println(TimesMat[1,kl]:0.01:TimesMat[1,kl]+TimesDict[kl])
-        #println(machines[out[1]]*ones(1,length(TimesMat[1,kl]:0.01:TimesMat[1,kl]+TimesDict[kl])))
         plot(collect(TimesMat[1,kl]+0.01:0.01:TimesMat[1,kl]+TimesDict[kl]-0.01),vec(machines[out[1]]*ones(1,length(TimesMat[1,kl]+0.01:0.01:TimesMat[1,kl]+TimesDict[kl]-0.01))))
         text((2*TimesMat[1,kl]+TimesDict[kl]) / 2, machines[out[1]], string(kl))
     end
-
     for kl in R
         out = (findall(x -> x == kl, Jobs))
-        #println(TimesMat[1,kl]:0.01:TimesMat[1,kl]+TimesDict[kl])
-        #println(machines[out[1]]*ones(1,length(TimesMat[1,kl]:0.01:TimesMat[1,kl]+TimesDict[kl])))
         plot(collect(maxtime-TimesMatInv[end,kl]+0.01:0.01:maxtime-TimesMatInv[end,kl]+TimesDict[kl]-0.01),vec(machines[out[1]]*ones(1,length(maxtime-TimesMatInv[end,kl]+0.01:0.01:maxtime-TimesMatInv[end,kl]+TimesDict[kl]-0.01))))
         text((maxtime-TimesMatInv[end,kl]+TimesDict[kl]-0.01+maxtime-TimesMatInv[end,kl]) / 2, machines[out[1]], string(kl))
-
+        #sleep(2)
     end
+    
 
 
 
 
 
-    #=
-            println(a)
-            a = a + 1
-
-        end
-
-        for j = 1:njobs
-            for i = 1:noperations
-
-
-                A = Sol[Jobs[j, i], :]
-
-                pos = findall(x -> x == 2, A)
-
-                ln = length(pos)
-                println(Jobs[j, i])
-
-
-                for op = 1:ln
-
-                    Sol[S[j][i], pos[op]] = 1
-                    Sol[pos[op], S[j][i]] = 0
-                    #println(pos[op])
-                    #Sol[j, pos[i_inv]] = 1
-                    #Sol[pos[i_inv], j] = 0
-                    #i_inv = i_inv - 1
-                end
-                append!(L, S[j][i])
-                remove!(T[j], S[j][i])
-                remove!(S[j], S[j][i])
-
-                if (length(L) + length(R) != size(Sol, 2))
-
-                    println("Cont")
-                end
-            end
-
-        end
-        println(L)
-        println(T)
-        println(S)
-        println("ok")
-        @show Sol
-
-    =#
+    println(string("MaxTime ",maxtimeNew))
 
 end
 
